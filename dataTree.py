05e00f7a5850d8054f203fec5832891671f475f0
Cromzst <CMZSrost@163.com>
2023-08-16 12:59:46 +0800

    0.5: 添加了右键新增拷贝table。

--- dataTree.py
+++ dataTree.py
@@ -1,18 +1,22 @@
 from PyQt5.QtWidgets import QTreeWidget, QTreeWidgetItemIterator, QMenu, QTreeWidgetItem, QAction
 from PyQt5.QtCore import Qt
 
+from tableDialog import tableDialog
+
+
 class dataTree(QTreeWidget):
     def __init__(self, parent=None):
         super(dataTree, self).__init__(parent)
         self.customContextMenuRequested.connect(self.open_menu)
         self.addTableAction = QAction('新增table', self)
+        self.copyTableAction = QAction('拷贝table', self)
         self.delTableAction = QAction('删除table', self)
         self.addTableAction.setObjectName('addTableAction')
+        self.copyTableAction.setObjectName('copyTableAction')
         self.delTableAction.setObjectName('delTableAction')
         self.addTableAction.triggered.connect(self.add_table)
+        self.copyTableAction.triggered.connect(self.copy_table)
         self.delTableAction.triggered.connect(self.del_table)
-
-
 
     def open_menu(self, pos):
         menu = QMenu()
@@ -22,12 +26,20 @@ class dataTree(QTreeWidget):
                 menu.addAction(self.addTableAction)
             elif self.currentItem().text(0) == 'table':
                 menu.addAction(self.delTableAction)
+                menu.addAction(self.copyTableAction)
             menu.exec_(self.mapToGlobal(pos))
 
     def add_table(self):
-        # item: QTreeWidgetItem = self.currentItem()
-        # item.addChild(QTreeWidgetItem(item, ['table']))
-        pass
+        item: QTreeWidgetItem = self.currentItem()
+        dialog = tableDialog(item)
+        dialog.exec_()
+
+
+    def copy_table(self):
+        item: QTreeWidgetItem = self.currentItem()
+        dialog = tableDialog(item.parent())
+        dialog.set_init(item)
+        dialog.exec_()
 
     def del_table(self):
         item: QTreeWidgetItem = self.currentItem()
@@ -37,8 +49,6 @@ class dataTree(QTreeWidget):
             item.removeChild(i)
         item.parent().removeChild(item)
         self.itemChanged.emit(item, 0)
-
-
 
     def filter_file(self, name):
         it = QTreeWidgetItemIterator(self.topLevelItem(0))
